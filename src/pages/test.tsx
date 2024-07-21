6846ae162a527f3820d2eeb2c06d1d03b6b4ba40
Jamil Loreto <jamil20002302@gmail.com>
2024-07-20 23:12:52 -0500

    fix date #2

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,36 +1,15 @@
 export default function Page() {
   const date = new Date().toLocaleDateString();
-  const date1 = new Date(date).toLocaleDateString("es", {
-    day: "numeric",
-    month: "long",
-  });
-  const date2 = new Date().toLocaleDateString("es", {
-    day: "numeric",
-    month: "long",
-  });
-  const date3 = new Date(new Date(date).toISOString()).toLocaleDateString(
-    "es",
-    {
-      day: "numeric",
-      month: "long",
-    }
-  );
-  const date4 = new Date(new Date(date)).toLocaleDateString("es", {
-    day: "numeric",
-    month: "long",
-  });
+
   return (
     <div>
       <p>Test</p>
-      <span>{date}</span>
-      <p>----</p>
-      <span>{date1}</span>
-      <p>--------</p>
-      <span>{date2}</span>
-      <p>--------</p>
-      <span>{date3}</span>
-      <p>---------</p>
-      <span>{date4}</span>
+      <span>
+        {new Date(new Date(date)).toLocaleDateString("es", {
+          day: "2-digit",
+          month: "long",
+        })}
+      </span>
     </div>
   );
 }
