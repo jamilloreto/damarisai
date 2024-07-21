492b7da6e42ca84faedcc3ac48d1929b1d6a5cc9
Jamil Loreto <jamil20002302@gmail.com>
2024-07-20 22:55:23 -0500

    fix date

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -8,6 +8,17 @@ export default function Page() {
     day: "numeric",
     month: "long",
   });
+  const date3 = new Date(new Date(date).toISOString()).toLocaleDateString(
+    "es",
+    {
+      day: "numeric",
+      month: "long",
+    }
+  );
+  const date4 = new Date(new Date(date)).toLocaleDateString("es", {
+    day: "numeric",
+    month: "long",
+  });
   return (
     <div>
       <p>Test</p>
@@ -16,6 +27,10 @@ export default function Page() {
       <span>{date1}</span>
       <p>--------</p>
       <span>{date2}</span>
+      <p>--------</p>
+      <span>{date3}</span>
+      <p>---------</p>
+      <span>{date4}</span>
     </div>
   );
 }
