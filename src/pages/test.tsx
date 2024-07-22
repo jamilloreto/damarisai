2a198edbcf0c5b295dbadaaa96735c06d1a08de5
Jamil Loreto <jamil20002302@gmail.com>
2024-07-21 22:44:05 -0500

    fix date #7

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -5,26 +5,19 @@ dayjs.extend(localizedFormat).locale("es");
 
 export default function Page() {
   const date = new Date().toLocaleDateString();
-  const parts = date.split("/");
-  const mm = parts[0].length === 1 ? `0${parts[0]}` : parts[0];
-  const newFormat = `${parts[2]}-${mm}-${parts[1]}`;
+  const newDate = new Date(date);
   return (
     <div>
       <p>Test 5</p>
       <span>{date}</span>
-      <span>
-        {new Date().toLocaleDateString("es", {
-          day: "2-digit",
-          month: "long",
-        })}
-      </span>
+
       <p>----</p>
-      <span>
-        {new Date(newFormat).toLocaleDateString("es", {
+      <b>
+        {new Date(newDate).toLocaleDateString("es", {
           day: "2-digit",
           month: "long",
         })}
-      </span>
+      </b>
     </div>
   );
 }
